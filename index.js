6098ed2783896077d2a3cacb40ef2538844fcec5
Francis Jacob <dev.francisjacob@gmail.com>
2023-04-06 03:40:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var imageSource1 = "images/dice" + randomNumber1 + ".png";
 
-document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
-
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
 var imageSource2 = "images/dice" + randomNumber2 + ".png";
 
-document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
+
+document.querySelector("button").addEventListener("click", function(){
+    document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
+    winner();
+})
+    
+document.querySelector("button").addEventListener("click", function(){
+    document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
+    winner();
+})
 
 
-
-        // COMPLEX METHOD
-
-/*   for (let i = 1; i <= 2; i++) {
-    var randomNumber = Math.floor(Math.random() * 6) + 1;
-    if (i === 1) {
-       var randomNumber1 = randomNumber;
-       var imageSource1 = "images/dice" + randomNumber1 + ".png";
-       document.querySelectorAll("img")[0].setAttribute("src", imageSource1);  
-    }else {
-        var randomNumber2 = randomNumber;
-        var imageSource2 = "images/dice" + randomNumber2 + ".png";
-        document.querySelectorAll("img")[1].setAttribute("src", imageSource2)
-        console.log(randomNumber2);
+function winner() {
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerText = "Player 1 wins";
+    } else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerText = "Player 2 wins";
+    }else{
+        document.querySelector("h1").innerText = "Draw";
     }
-}        
-    */
-
-
-
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerText = "Player 1 wins";
-} else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerText = "Player 2 wins";
-}else{
-    document.querySelector("h1").innerText = "Draw";
-} 
+}
