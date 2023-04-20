938125de537a177389184eb856e0a76da170f915
Francis Jacob <dev.francisjacob@gmail.com>
2023-04-20 08:25:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var randomNumber2
 
 document.querySelector("button").addEventListener("click", function(){
     randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    var imageSource1 = "images/dice" + randomNumber1 + ".png";
+    const imageSource1 = "images/dice" + randomNumber1 + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
     winner();
 });
     
 document.querySelector("button").addEventListener("click", function(){
     randomNumber2 = Math.floor(Math.random() * 6) + 1;
-    var imageSource2 = "images/dice" + randomNumber2 + ".png";
+    const imageSource2 = "images/dice" + randomNumber2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
     winner();
 });
